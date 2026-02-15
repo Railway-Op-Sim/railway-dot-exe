8dc9c398dbe7d042e596f7df1f7263565157b4cb
Kristian Zarębski <krizar312@gmail.com>
2026-02-15 09:32:21 +0000

    Started metadata component

--- railway.cpp
+++ railway.cpp
@@ -4,10 +4,10 @@
 #pragma hdrstop
 #include <tchar.h>
 //---------------------------------------------------------------------------
-USEFORM("..\..\GitHub Current Version\railway-dot-exe\PerfLogUnit.cpp", PerfLogForm);
-USEFORM("..\..\GitHub Current Version\railway-dot-exe\ActionsDueUnit.cpp", ActionsDueForm);
-USEFORM("..\..\GitHub Current Version\railway-dot-exe\AboutUnit.cpp", AboutForm);
-USEFORM("..\..\GitHub Current Version\railway-dot-exe\InterfaceUnit.cpp", Interface);
+USEFORM("PerfLogUnit.cpp", PerfLogForm);
+USEFORM("ActionsDueUnit.cpp", ActionsDueForm);
+USEFORM("AboutUnit.cpp", AboutForm);
+USEFORM("InterfaceUnit.cpp", Interface);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
@@ -19,10 +19,10 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
         /*NB CreateForm() runs the form's constructor, so if another form is referenced in it that form must be created first,
         e.g. Interface before AboutForm (although InterfaceUnit.cpp includes AboutUnit.h the constructor doesn't reference it). */
          Application->CreateForm(__classid(TInterface), &Interface);
-         Application->CreateForm(__classid(TPerfLogForm), &PerfLogForm);
-         Application->CreateForm(__classid(TAboutForm), &AboutForm);
-         Application->CreateForm(__classid(TActionsDueForm), &ActionsDueForm);
-         Application->Run();
+		Application->CreateForm(__classid(TPerfLogForm), &PerfLogForm);
+		Application->CreateForm(__classid(TAboutForm), &AboutForm);
+		Application->CreateForm(__classid(TActionsDueForm), &ActionsDueForm);
+		Application->Run();
     }
     catch (Exception &exception)
     {
